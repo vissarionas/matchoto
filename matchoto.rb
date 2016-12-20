335c9902c9e46a5ec2f054f0d235f1803b22efd4
vissarionas <vissarionas@gmail.com>
2016-12-20 17:10:12 +0200

    caption activities

--- matchoto.rb
+++ matchoto.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 require 'fileutils'
 
-Shoes.app(title: "matchoto", width: 1000, height: 200, resizable: false) do
+Shoes.app(title: "matchoto", width: 1000, height: 500, resizable: false) do
 
   @all_files = []
   @current_val = nil
@@ -10,49 +10,64 @@ Shoes.app(title: "matchoto", width: 1000, height: 200, resizable: false) do
   @select_btn = nil
   @product_code = nil
   @rename_folder = nil
+  @db_view = nil
+  @time = Time.new.inspect
 
-  button "New Job" do
-    if confirm ("Starting a new job, will erase any previous work. \nAre you sure?")
-      place_ok_folder
-      delete_db_if_exists
-      @select_btn.show()
+
+  @control_flow = flow do
+    button "view database" do
+      view_db
+    end
+
+    button "New Job" do
+      if confirm ("Starting a new job, will erase any previous work. \nAre you sure?")
+        place_ok_folder
+        delete_db_if_exists
+        @select_btn.show()
+        @report_view=caption @time+" --> Created folders 'OK' and 'renamed'. Deleted previous database\n"
+      end
+    end
+
+    @select_btn = button "Select folder" do
+      @working_folder = ask_open_folder
+      if !check_folder(@working_folder)
+        alert("Selected folder contains previous photos. \nPlease format before starting a new job")
+        @report_view = caption @time+" --> Working folder is not empty. Please empty working folder\n"
+      else
+        delete_db_if_exists
+    	  create_database
+        @select_btn.hide()
+        @report_view = caption @time+" --> Previous database deleted. New database created. Selected working folder is "+@working_folder+"\n"
+      end
+  	end
+
+    # button "Import TextFile" do
+    # 	import_txt
+    # end
+
+    button "New code" do
+      write_to_db
+      @product_code = ask("Enter product code:")
+      @select_btn.hide
+      @report_view = caption @time+" --> Product code = "+@product_code+".\n"
+    end
+
+    # button "Edit code" do
+    #   @product_code << ask("Edit your code:")
+    # end
+
+    button "Finish" do
+      write_to_db
+      @report_view = caption @time+" --> Job is finished."
+    end
+
+    button "Rename" do
+      rename
     end
   end
 
-  @select_btn = button "Select folder" do
-    @working_folder = ask_open_folder
-    if !check_folder(@working_folder)
-      alert("Selected folder contains previous photos. \nPlease format before starting a new job")
-    else
-      delete_db_if_exists
-  	  create_database
-      @select_btn.hide()
-      debug(@working_folder)
-    end
-	end
-
-  # button "Import TextFile" do
-  # 	import_txt
-  # end
-
-  button "New code" do
-    write_to_db
-    @product_code = ask("Enter product code:")
-    display_current_val
-    @select_btn.hide
-  end
-
-  # button "Edit code" do
-  #   @product_code << ask("Edit your code:")
-  # end
-
-  button "Finish" do
-    write_to_db
-    # File.rename(@folder + '/' + @all_files[index].first, @folder + '/' + code + '.jpg')
-  end
-
-  button "Rename" do
-    rename
+  @caption_flow = flow do
+   @report_view
   end
 
   def place_ok_folder
@@ -64,13 +79,13 @@ Shoes.app(title: "matchoto", width: 1000, height: 200, resizable: false) do
     Dir.mkdir 'OK/renamed'
   end
 
-	def display_current_val
-    flow top: 50, left: 0 do
-      caption "Current code:", stroke: "#fff"
-      @current_val.remove unless @current_val == nil
-      @current_val = caption @product_code, stroke: "#fff"
-    end
-  end
+	# def display_current_val
+  #   flow top: 50, left: 0 do
+  #     caption "Current code:", stroke: "#fff"
+  #     @current_val.remove unless @current_val == nil
+  #     @current_val = caption @product_code, stroke: "#fff"
+  #   end
+  # end
 
   def existing_files
     results = @db.execute "select filename from pairs"
@@ -125,7 +140,7 @@ Shoes.app(title: "matchoto", width: 1000, height: 200, resizable: false) do
         if files.include?(file[0])
           FileUtils.cp('OK/'+file[0] , 'OK/renamed/'+file[0])
           FileUtils.mv('OK/renamed/'+file[0] , 'OK/renamed/'+file[1]+'.jpg')
-          debug ("OK/renamed/"+file[0] +"renamed to" + "OK/renamed/"+file[1]+".jpg")
+          @report_view = caption "OK/renamed/"+file[0] +"renamed to" + "OK/renamed/"+file[1]+".jpg"
         end
       end
     else
@@ -133,4 +148,17 @@ Shoes.app(title: "matchoto", width: 1000, height: 200, resizable: false) do
     end
   end
 
+  def view_db
+    window title: "database contents" do
+      files = Dir.entries('.')
+      if files.include?('default.db')
+        @db = SQLite3::Database.new 'default.db'
+        @db.execute("select filename , product_code from pairs").each do |file|
+          @db_view = caption (file[0] +" > "+file[1]+"\n")
+        end
+      else
+        @db_view = caption "empty/no database"
+      end
+    end
+  end
 end
